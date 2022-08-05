796ea55bdfac081f0f5b2ddcf30fbb6a0e0616a5
Aina <ainamoreno@outlook.com>
2022-08-05 18:21:20 +0200

    funciones cálculos

--- codigo.js
+++ codigo.js
@@ -2,8 +2,10 @@ const botonNumeros = document.getElementsByName('numero');
 const botonOperadores = document.getElementsByName('operadores');
 const botonIgual = document.getElementById('operador-igual')
 const botonDelate = document.getElementById('operador-delate')
+pantalla = document.getElementById('pantalla')
 let opeActual = ''
 let opeAnterior = ''
+let operacion = undefined
 console.log(botonNumeros)
 
 botonNumeros.forEach(numeros = (boton) => {
@@ -15,35 +17,76 @@ botonNumeros.forEach(numeros = (boton) => {
 
 botonOperadores.forEach(numeros = (boton) => {
     boton.addEventListener('click', num = () => {
-        mostrarEnPantalla(boton.innerText);
+        seleccionarOperacion(boton.innerText);
     })
 
 })
 
 
 botonIgual.addEventListener('click', igual = () => {
-    calculador()
+    calculador();
+    actualizarPantalla();
 })
 
 botonDelate.addEventListener('click', delate = () => {
-    limpiar()
+    limpiar();
+    actualizarPantalla()
 })
 
 
 
 mostrarEnPantalla = (num) => {
-    pantalla = document.getElementById('pantalla')
     pantalla.innerHTML += `<div>  
    ${opeActual = opeActual.toString() + num.toString()}
-   ${pantalla.value = opeActual}
+   ${actualizarPantalla()}
    </div`
+}
+
+actualizarPantalla = () => {
+    pantalla.innerHTML += `<div>  
+    ${pantalla.value = opeActual}
+    </div`
+}
+
+seleccionarOperacion = (op) => {
+    if (opeActual === '') return
+    if (opeActual !== '') {
+        calculador()
+    }
+    operacion = op.toString();
+    opeAnterior = opeActual;
+    opeActual = '';
 }
 
 
 limpiar = () => {
-    opeActual = ''
-    opeAnterior = ''
-}
-calculador = () => {
+    opeActual = '';
+    opeAnterior = '';
+
 
 }
+calculador = () => {
+    let calculo
+    const actual = Number(opeActual);
+    const anterior = Number(opeAnterior);
+    if (isNaN(actual) || isNaN(anterior)) return
+    switch (operacion) {
+        case '+':
+            calculo = anterior + actual
+            break;
+        case '-':
+            calculo = anterior - actual
+            break;
+        case '/':
+            calculo = anterior / actual
+            break;
+        case 'x':
+            calculo = anterior * actual
+            break;
+        default:
+            return;
+    }
+    opeActual = calculo
+}
+
+
